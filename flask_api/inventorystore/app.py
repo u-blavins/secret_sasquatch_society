6436919df1a33033d6154e29e4aa839c536f4b9c
u-blavins <usama.blavins1@gmail.com>
2020-01-06 22:09:12 +0800

    Add files for web application

---
+++ flask_api/inventorystore/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/<name>')
+def greet(name):
+    return "Hello {}!".format(name)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
